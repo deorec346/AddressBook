6e095cd7e697e97dab44c39853f04ab8a51add65
Chetan Deore <deorec346@gmail.com>
2022-05-07 16:10:27 +0530

    EditingContact

---
+++ AddressBookProgram/AddressBookCompute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    class AddressBookCompute
+    {
+
+        private LinkedList<ContactDetails> contactList;
+
+        public AddressBookCompute()
+        {
+            this.contactList = new LinkedList<ContactDetails>();
+        }
+
+        //this method add details to the address book
+        public void AddContactDetails(string firstName, string lastName, string address, string city, string state, long zipCode, long phoneNumber)
+        {
+            ContactDetails contactDetails = new ContactDetails(firstName, lastName, address, city, state, zipCode, phoneNumber);
+            this.contactList.AddLast(contactDetails);
+        }
+
+        public void DisplayContact()
+        {
+            foreach (ContactDetails contact in this.contactList)
+            {
+                contact.Display();
+            }
+        }
+        public void EditContact(string name, long number)
+        {
+            //checks for every object whether the name is equal the given name
+            foreach (ContactDetails contact in this.contactList)
+            {
+                if (contact.firstName.Equals(name))
+                {
+                    //calls the setdetail method
+                    contact.SetDetail(number);
+                }
+            }
+        }
+
+    }
+}

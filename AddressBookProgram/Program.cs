fb75384715309975d42ed905e373f1393b8683fa
Chetan Deore <deorec346@gmail.com>
2022-05-07 15:56:22 +0530

    CreateContact

---
+++ AddressBookProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AddressBookProgram
+{
+    class Program
+    {
+        /// <summary>
+        /// Welcome to Address Book Management System
+        /// //UC1-C
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+      
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to Address book");
+                //calling operation management class that contains operation
+                OperationManagement.AddDetails();
+                Console.Read();
+            }
+        }
+    }
+

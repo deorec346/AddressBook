ae4fc3134bce8aab1805a7824d0b43503500dab8
Chetan Deore <deorec346@gmail.com>
2022-05-07 19:51:49 +0530

    NoDuplicateEntry

---
+++ AddressBookProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AddressBookProgram
+{
+    class Program
+    {
+        /// <summary>
+        /// Welcome to Address Book Management System
+        /// UC1 - Gets the contact detail
+        /// UC2- Get and stores multiple contacts
+        /// UC3- Edit the contacts
+        /// UC4-Deleting the particular contact detail
+        /// UC5 -Add multiple person to address book one by one from user
+        /// UC6- Adding Multiple Address Book
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address book");
+            //calling operation management class that contains operation
+            OperationManagement operationManagement = new OperationManagement();
+
+            //non-static method 
+            operationManagement.ReadInput();
+            Console.Read();
+        }
+    }
+}
